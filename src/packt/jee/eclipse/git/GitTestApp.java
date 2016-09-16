01bdf4200e43377e2a0c6ee95e55b56ca445830e
Joseph Fletcher <joseph.fletcher@cognizant.com>
2016-09-16 13:46:39 -0400

    added my old friend

--- src/packt/jee/eclipse/git/GitTestApp.java
+++ src/packt/jee/eclipse/git/GitTestApp.java
@@ -3,6 +3,6 @@ package packt.jee.eclipse.git;
 public class GitTestApp {
 	public static void main(String[] args) {
 	  // do something	
-		System.out.println("Hello Git, from branch bug#1234 !!");
+		System.out.println("Hello Git my old friend, from branch bug#1234 !!");
 	}
 }
