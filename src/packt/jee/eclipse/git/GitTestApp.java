d96063a6bb5a5fa22110392d2d6aeee88a77bd97
Joseph Fletcher <joseph.fletcher@cognizant.com>
2016-09-16 13:06:13 -0400

    bug#1234 changes

--- src/packt/jee/eclipse/git/GitTestApp.java
+++ src/packt/jee/eclipse/git/GitTestApp.java
@@ -3,6 +3,6 @@ package packt.jee.eclipse.git;
 public class GitTestApp {
 	public static void main(String[] args) {
 	  // do something	
-		System.out.println("Hello There!!");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 }
